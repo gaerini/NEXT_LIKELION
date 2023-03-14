1c20fd88e21b24b710916a3f6d9d0a60e8be6c3f
jiho Kim <jimmy7335@gmail.com>
2023-03-14 12:47:34 +0900

    session2

---
+++ 교육자료/Session2/mini1.py
@@ -0,0 +1,20 @@
+# word = input()
+
+# if word == '고양이':
+#     print('야옹')
+
+# elif word == '김지욱':
+#     print('안녕하세요. 만나서 반갑습니다.')
+
+# else:
+#     print(f'나는 {word}가 아니라 알락꼬리꼬마도요야.')
+
+animal = input()
+
+if animal == '고양이':
+    print('야옹')
+
+elif animal == '김지욱':
+    print('ㅎㅇ')
+else:
+    print(f'나는 {animal}이/가 아니라 알락이야')
